49b2381f0e326e53a20883dcf52050ca4ee362f2
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-05-09 21:30:30 +0800

    :bookmark: 0.1.0-rc.patch03 add help cmd

--- src/cmd/help.ts
+++ src/cmd/help.ts
@@ -1,4 +1,4 @@
-import {Context, Logger} from "koishi";
+import {Context, h, Logger} from "koishi";
 import {Config} from "../config";
 import {APIService} from "../service";
 
@@ -6,9 +6,13 @@ export function HelpCmd(ctx:Context,cfg:Config,api:APIService,logger:Logger) {
   const cmpcmd = ctx
     .command('bsbot.help')
     .alias('bbhelp')
+    .alias('!help')
     .action(async ({ session, options }, input) => {
-    //   todo
-      // send a help img
-      session.send("still working on")
+      await session.sendQueued(h('image', {
+        src: 'https://tmp-r2.ktlab.io/bsbot.basic.v0.1.0.png'
+      }))
+      await session.sendQueued(h('image', {
+        src: 'https://tmp-r2.ktlab.io/bsbot.sub.v0.1.0.png'
+      }))
     })
 }
